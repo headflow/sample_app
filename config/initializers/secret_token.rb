fa9f9a96fdda2c6a7487ca8b618724c11e1ad985
HeadFlow <info@headflow.net>
2010-12-29 01:08:35 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4a1217d3588c8ec4910b036cfaa349596cb346e0dbca14338b8feecdb4cc0ce2ac84a1b690c85673c621678be91c50fd16cea62e0747f0191b891c31d32db819'
